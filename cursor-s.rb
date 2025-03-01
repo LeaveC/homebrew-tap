6551dda72f113f25cc4da689bd417fe927b35d67
LeaveC <1139686729@qq.com>
2025-03-01 18:22:39 +0800

    Update formula to use source code directly

--- cursor-s.rb
+++ cursor-s.rb
@@ -1,29 +1,21 @@
 class CursorS < Formula
   desc "Cursor Auth System Client"
   homepage "https://github.com/LeaveC/cursor-auth-system"
+  url "https://github.com/LeaveC/cursor-auth-system.git"
   version "1.0.0"
-  
-  if OS.mac?
-    url "https://github.com/LeaveC/cursor-auth-system/releases/download/v1.0.0/cursor-s-darwin-amd64.tar.gz"
-    sha256 "YOUR_DARWIN_SHA256"
-  elsif OS.linux?
-    url "https://github.com/LeaveC/cursor-auth-system/releases/download/v1.0.0/cursor-s-linux-amd64.tar.gz"
-    sha256 "YOUR_LINUX_SHA256"
-  end
 
   depends_on "go" => :build
 
   def install
-    cd "cursor-auth-system/go-client" do
-      system "go", "build", "-o", "cursor-s"
-      bin.install "cursor-s"
-      prefix.install "go.mod"
-      prefix.install "go.sum"
-      prefix.install ".env"
-      prefix.install "Icon.png"
-      prefix.install Dir["cursor_auth"]
-      prefix.install "main.go"
-    end
+    # 创建一个简单的启动脚本
+    (bin/"cursor-s").write <<~EOS
+      #!/bin/bash
+      cd #{prefix}/go-client && go run main.go "$@"
+    EOS
+
+    # 复制源代码
+    prefix.install Dir["*"]
+    chmod 0755, bin/"cursor-s"
   end
 
   test do
