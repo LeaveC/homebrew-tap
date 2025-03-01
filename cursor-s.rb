98b825ab7843ef931a8888bde3e6c89b0d85cb8d
LeaveC <1139686729@qq.com>
2025-03-01 18:23:46 +0800

    Update formula to use git HEAD

--- cursor-s.rb
+++ cursor-s.rb
@@ -1,21 +1,23 @@
 class CursorS < Formula
   desc "Cursor Auth System Client"
   homepage "https://github.com/LeaveC/cursor-auth-system"
-  url "https://github.com/LeaveC/cursor-auth-system.git"
+  head "https://github.com/LeaveC/cursor-auth-system.git", branch: "main"
   version "1.0.0"
 
   depends_on "go" => :build
 
   def install
-    # 创建一个简单的启动脚本
-    (bin/"cursor-s").write <<~EOS
-      #!/bin/bash
-      cd #{prefix}/go-client && go run main.go "$@"
-    EOS
+    cd "go-client" do
+      # 创建一个简单的启动脚本
+      (bin/"cursor-s").write <<~EOS
+        #!/bin/bash
+        cd #{prefix}/go-client && go run main.go "$@"
+      EOS
 
-    # 复制源代码
-    prefix.install Dir["*"]
-    chmod 0755, bin/"cursor-s"
+      # 复制源代码到安装目录
+      (prefix/"go-client").install Dir["*"]
+      chmod 0755, bin/"cursor-s"
+    end
   end
 
   test do
