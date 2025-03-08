e25214df5792c08de839d6e3e22b91131b6c9c08
LeaveC <1139686729@qq.com>
2025-03-08 19:04:19 +0800

    Update PySide6 package URL and SHA256

--- Formula/client-pk.rb
+++ Formula/client-pk.rb
@@ -10,18 +10,18 @@ class ClientPk < Formula
     depends_on "python@3.11"
     
     resource "pyside6" do
-      url "https://files.pythonhosted.org/packages/PySide6-6.6.1.tar.gz"
-      sha256 "YOUR_PYSIDE6_SHA256"
+      url "https://files.pythonhosted.org/packages/py3/P/PySide6/PySide6-6.6.1-cp38-abi3-macosx_11_0_universal2.whl"
+      sha256 "3c348948fe3957b18164c9c7b8942fe065bdb39648b326f212bc114326679fa9"
     end
   
     resource "requests" do
-      url "https://files.pythonhosted.org/packages/requests-2.31.0.tar.gz"
-      sha256 "YOUR_REQUESTS_SHA256"
+      url "https://files.pythonhosted.org/packages/source/r/requests/requests-2.31.0.tar.gz"
+      sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
     end
   
     resource "python-dotenv" do
-      url "https://files.pythonhosted.org/packages/python-dotenv-1.0.0.tar.gz"
-      sha256 "YOUR_DOTENV_SHA256"
+      url "https://files.pythonhosted.org/packages/source/p/python-dotenv/python-dotenv-1.0.0.tar.gz"
+      sha256 "a8df96034aae6d2d50a4ebe8216326c61c3eb64836776504fcca410e5937a3ba"
     end
   
     def install
